5b5e80ec6cd42c471c251d02ebdbe87e474a298f
Abbby1007 <Abigailloken25@gmail.com>
2026-06-25 11:37:36 -0400

    Adding the Global script

---
+++ global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var minigames_done = 0 # Track the amount of minigames completed
+var lives = 5 # track how mmay lives is left, also effecting garlic appearing
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
